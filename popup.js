ff0ae21527e7e56bd266e04675825341d5ec1245
Nigel Davis <ngeldvis@gmail.com>
2021-07-02 15:51:28 -0400

    began work on option page ui

---
+++ popup.js
@@ -0,0 +1,9 @@
+let optionsLink = document.getElementById('options-link');
+
+optionsLink.addEventListener('click', () => {
+    if (chrome.runtime.openOptionsPage) {
+      chrome.runtime.openOptionsPage();
+    } else {
+      window.open(chrome.runtime.getURL('options.html'));
+    }
+});
